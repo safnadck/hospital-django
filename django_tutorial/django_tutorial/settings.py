5970580994327e4d9b3c47a74d2f6a3e417349e8
muhammed safnad <muhammedsafnadck5@gmail.com>
2024-12-30 20:13:40 +0530

    30-12-2024

--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/media/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
--- django_tutorial/home/views.py
+++ django_tutorial/home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Departments
+from .forms import BookingForm
+
+from .models import Departments, Doctors
 
 def index(request):
     return render(request, 'index.html')
@@ -10,10 +12,24 @@ def about(request):
     return render(request, 'about.html')
 
 def booking(request):
-    return render(request, 'booking.html')
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'confirmation.html')
+                    
+    form = BookingForm()
+    dict_form={
+        'form' : form
+    }
+    return render(request, 'booking.html', dict_form)
+
 
 def doctors(request):
-    return render(request, 'doctors.html')
+    dict_docs = {
+     'doctors': Doctors.objects.all()
+    }
+    return render(request, 'doctors.html', dict_docs)
 
 def contact(request):
     return render(request, 'contact.html')
